2b42ad65c87b0abf1ac6a137a94e7ce7d09f92f7
Maximov1233 <52001281+Maximov1233@users.noreply.github.com>
2020-02-02 14:40:26 +0600

    crossCell, boxCell, => switcher

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
-const box    = document.querySelector('.box');
+const box = document.querySelector('.box');
+
+// addind cells into box
     
 for (let i = 0; i < 100; i++) {
     const boxCell = document.createElement('div');
     boxCell.classList.add('box-cell');
     box.appendChild(boxCell);
 }
+const boxCells = box.querySelectorAll('.box-cell');
+let current = 'block';
 
+const cross   = document.querySelector('.cross-wrap'),
+    block     = document.querySelector('.block-wrap'),
+    stickCell = document.createElement('div'),
+    crossCell = document.createElement('div');
 
-    boxCells = box.querySelectorAll('.box-cell');
+crossCell.classList.add('cross-cell');
+stickCell.classList.add('stick-cell');
+
+crossCell.appendChild(stickCell);
+const stickCellClone = stickCell.cloneNode(true);
+crossCell.appendChild(stickCellClone);
 
 boxCells.forEach((cell) => {
     
     cell.addEventListener('click', () => {
-        cell.style.background = '#334861';
-        cell.style.borderColor = '#24344B';
-        // cell.style.border = '0';
-        // cell.style.width = '152px';
-        // cell.style.height = '150px';
+        if (current === 'block') {
+            cell.style.background = '#334861';
+            cell.style.borderColor = '#24344B';
+            console.log('a');
+        } else if (current == 'cross') {
+            const crossCellClone = crossCell.cloneNode(true);
+            cell.appendChild(crossCellClone);
+        }
     });
 });
 
+// giving coordinates
+
 let x = 1,
     y = 1;
+
 for (let i = 0; i < boxCells.length; i++) {
     boxCells[i].dataset.x = x;
     boxCells[i].dataset.y = y;
@@ -33,4 +52,18 @@ for (let i = 0; i < boxCells.length; i++) {
     }
 }
 
+// switcher
 
+block.style.background = '#fff';
+
+cross.addEventListener('click', () => {
+    cross.style.background = '#fff';
+    block.style.background = '';
+    current = 'cross';
+});
+
+block.addEventListener('click', () => {
+    cross.style.background = '';
+    block.style.background = '#fff';
+    current = 'block';
+});
